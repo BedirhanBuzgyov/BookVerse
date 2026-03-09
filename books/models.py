1e3f2301392fcf4f0229ab7990fcd58b124b9afc
BedirhanBuzgyov <bedirhanbuzgyov@gmail.com>
2026-03-09 18:12:20 +0200

    chore: added models in books, authors, genres apps

--- books/models.py
+++ books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from authors.models import Author
+from genres.models import Genre
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    genres = models.ManyToManyField(Genre)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
