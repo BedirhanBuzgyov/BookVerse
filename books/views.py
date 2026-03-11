0ea99d2850956bce1d7710f997e01d8144a48b55
BedirhanBuzgyov <bedirhanbuzgyov@gmail.com>
2026-03-11 21:24:31 +0200

    chore: added views.py for the books app

--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def book_list(request: HttpRequest) -> HttpResponse:
+    books = Book.objects.all().select_related("author").prefetch_related("genres")
+
+    return render(request, "books/book_list.html", {"books": books})
+
