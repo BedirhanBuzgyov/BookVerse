a25941daf2587b3e002eb4fda3695d70836d79a0
BedirhanBuzgyov <bedirhanbuzgyov@gmail.com>
2026-03-15 15:57:58 +0200

    Add Delete/Edit features

--- books/urls.py
+++ books/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.book_list, name="book_list"),
     path("add/", views.book_create, name="book_create"),
-    path("<int:id>/", views.book_detail, name="book_detail")
+    path("<int:id>/", views.book_detail, name="book_detail"),
+    path("<int:id>/edit/", views.book_edit, name="book_edit"),
+    path("<int:id>/delete/", views.book_delete, name="book_delete"),
 ]
