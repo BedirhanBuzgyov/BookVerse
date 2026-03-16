ffe21051979b52bf864f795bbee1abadf8e39a4b
BedirhanBuzgyov <bedirhanbuzgyov@gmail.com>
2026-03-16 22:16:04 +0200

    Add delete-create-edit features

--- authors/urls.py
+++ authors/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 urlpatterns = [
     path("", author_list, name="author_list"),
-    path("<int:id>", author_detail, name="author_detail")
+    path("add/", author_create, name="author_create"),
+    path("<int:id>", author_detail, name="author_detail"),
+    path("<int:id>/edit/", author_edit, name="author_edit"),
+    path("<int:id>/delete/", author_delete, name="author_delete")
+
 ]
