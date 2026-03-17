0d9b7505f5478e9f1c51cbb13f1f8da071b603e1
BedirhanBuzgyov <bedirhanbuzgyov@gmail.com>
2026-03-17 11:37:27 +0200

    Add create/edit/delete features

--- genres/urls.py
+++ genres/urls.py
@@ -4,5 +4,8 @@ from genres.views import *
 
 urlpatterns = [
     path("", genre_list, name="genre_list"),
-    path("<int:id>", genre_detail, name="genre_detail")
+    path("add/", genre_create, name="genre_create"),
+    path("<int:id>/", genre_detail, name="genre_detail"),
+    path("<int:id>/edit", genre_edit, name="genre_edit"),
+    path("<int:id>/delete", genre_delete, name="genre_delete")
 ]
